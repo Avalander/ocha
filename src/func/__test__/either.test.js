78a46335d992b4e437fcc366919f07a910d42235
Avalander <darth.quesorico@gmail.com>
2018-07-07 22:12:38 +0200

    Add tests for Either.tryCatch and Either.conditional.

--- src/func/__test__/either.test.js
+++ src/func/__test__/either.test.js
@@ -1,6 +1,6 @@
 const test = require('tape')
 
-const { Either, Left, Right, tryCatch } = require('func/either')
+const { Either, Left, Right, tryCatch, conditional } = require('func/either')
 
 
 // Left
@@ -88,4 +88,49 @@ test('Right.fold should invoke right function with value.', t => {
 		x => t.fail('Invoked left function.'),
 		x => t.equal(x, 7)
 	)
+})
+
+
+// tryCatch
+
+const throwIfEven = x => {
+	if (x % 2 === 0) throw new Error(`I don't like even numbers`)
+	return x
+}
+
+test('tryCatch should return Right with value when function does not throw.', t => {
+	t.plan(2)
+
+	const actual = tryCatch(throwIfEven, 3)
+	t.equal(actual.prototype, Either.prototype)
+	t.equal(actual.inspect(), 'Right(3)')
+})
+
+test('tryCatch should return Left with error when function throws.', t => {
+	t.plan(2)
+
+	const actual = tryCatch(throwIfEven, 4)
+	t.equal(actual.prototype, Either.prototype)
+	t.equal(actual.inspect(), `Left(Error: I don't like even numbers)`)
+})
+
+
+// conditional
+
+const isEven = conditional(value => value % 2 === 0, 'Value is not even')
+
+test('conditional should return Right with value when it fulfills the condition.', t => {
+	t.plan(2)
+
+	const actual = isEven(4)
+	t.equal(actual.prototype, Either.prototype)
+	t.equal(actual.inspect(), 'Right(4)')
+})
+
+test('conditional should return Left with reason when it does not fulfill the condition.', t => {
+	t.plan(2)
+
+	const actual = isEven(3)
+	t.equal(actual.prototype, Either.prototype)
+	t.equal(actual.inspect(), 'Left(Value is not even)')
 })
